6df9a86747655949e7e0e4f1380bb4091198c024
Florian Schmidt <florian.schmidt@time4fun.de>
2018-01-12 06:49:09 +0100

    Customize root view controller

--- NonStoryboardApp/ViewController.swift
+++ NonStoryboardApp/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    fileprivate lazy var webView: WKWebView = {
+        let w = WKWebView()
+        w.translatesAutoresizingMaskIntoConstraints = false
+        return w
+    }()
+    
+    fileprivate lazy var infoLabel: UILabel = {
+        let l = UILabel()
+        
+        l.translatesAutoresizingMaskIntoConstraints = false
+        l.text = "No Storyboard"
+        l.font = UIFont.systemFont(ofSize: 17, weight: .black)
+        l.textAlignment = .center
+        
+        return l
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView.load(URLRequest(url: URL(string: "https://elmland.blog/2018/01/12/xcode-project-without-storyboard/")!))
+        
+        customizeView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func customizeView(){
+        view.backgroundColor = .white
+        
+        view.addSubview(infoLabel)
+        view.addSubview(webView)
+        
+        infoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        infoLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        
+        webView.topAnchor.constraint(equalTo: infoLabel.bottomAnchor, constant: 20).isActive = true
+        webView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        webView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        
     }
-
-
 }
 
